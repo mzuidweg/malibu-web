55e6e7c0d99deb207e5d40cb0eedc5d497e7aa66
mzuidweg <github@martijnzuidweg.nl>
2016-05-22 18:24:43 +0200

    clippie erop!

--- web/index.php
+++ web/index.php
@@ -66,7 +66,7 @@
     <div class="kopje">Boekingen / Contact</div>
     <div class="paragraaf">
       <a href="mailto:boekingen@malibumalibu.nl">boekingen@malibumalibu.nl</a><br/>
-      <a href="http://www.facebook.com/malibumuziek">facebook</a>
+      <a href="http://www.facebook.com/malibumuziek">facebook</a><br/>
       <a href="http://www.instagram.com/malibumalibumalibumalibu">instagram</a>
     </div>
   </div>
